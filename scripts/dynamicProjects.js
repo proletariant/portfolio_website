439517640936f50440639bd14bfaf5c2f59bfb4a
proletariant <proletariant@users.noreply.github.com>
2023-12-19 23:53:57 +1100

    fixed fetch moved project display to dynamic div

--- scripts/dynamicProjects.js
+++ scripts/dynamicProjects.js
@@ -1,43 +1,58 @@
 /* code iterates between items in JSON file to display projects */
-function populate(domain) {
-    jsonData = getJSON();
+async function populate(domain) {
 
-    const titleList = document.getElementsByClassName("title");
-    const descList = document.getElementsByClassName("description");
-    const linkList = document.getElementsByClassName("link");
-    var j = 0;
+    // receive JSON data
+    const jsonData = await getJSON();
+    console.log(jsonData);
+
+    // dynamic div setup
+    var projectView = document.getElementById("container");
+    while (projectView.firstChild) {
+        projectView.removeChild(projectView.firstChild);
+    }
 
     for (var i = 0; i < jsonData.projects.length; i++) {
         var obj = jsonData.projects[i];
+        var project = document.createElement("div");
+        project.classList.add("projects");
         for (var key in obj) {
             var attrName = key;
             var attrValue = obj[key];
+            console.log(attrName);
+            if (attrName == "type" && attrValue != domain && domain != null) break;
             /* send name value, description, photos, link (if applicable) */
             switch (attrName) {
-                case title:
-                    console.log("title");
-                    titleList[j].innerHTML = attrValue;
+                case "title":
+                    var title = document.createElement("h1");
+                    title.textContent = attrValue;
+                    project.appendChild(title);
                     break;
-                case description:
-                    descList[j].innerHTML = attrValue;
+                case "description":
+                    var desc = document.createElement("p");
+                    desc.textContent = attrValue;
+                    project.appendChild(desc);
                     break;
-                case link:
-                    linkList[j].innerHTML = attrValue;
+                case "link":
+                    var link = document.createElement("a");
+                    link.textContent = attrValue;
+                    link.setAttribute("href", attrValue);
+                    project.appendChild(link);
                     break;
                 default:
                     break;
             }
         }
-        j++;
+        project.appendChild(document.createElement("hr"));
+        projectView.appendChild(project);
     }
 }
 
+/* helper function to receive JSON file via fetch for inclusion */
+/* TODO: change fetch request to local path (URL is only for local testing) */
 async function getJSON() {
-    fetch('main.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+    const response = await fetch('https://proletariant.github.io/portfolio_website/scripts/main.json');
 
-    return json;
+    const data = response.json();
+
+    return data;
 }
-
-populate(1);
