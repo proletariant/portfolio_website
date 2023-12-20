79f112e1cce567df8aa74e7303ee168cf3afc2a6
proletariant <proletariant@users.noreply.github.com>
2023-12-20 18:05:01 +1100

    changed navbar structure to reduce page count + other stuff

--- scripts/dynamicProjects.js
+++ scripts/dynamicProjects.js
@@ -11,16 +11,21 @@ async function populate(domain) {
         projectView.removeChild(projectView.firstChild);
     }
 
+    // for each project in jsonList
     for (var i = 0; i < jsonData.projects.length; i++) {
+        // get current project
         var obj = jsonData.projects[i];
+        // create empty div element to fill with project information
         var project = document.createElement("div");
         project.classList.add("projects");
+        // for every project field in the current project
         for (var key in obj) {
+            // grab both key and value
             var attrName = key;
             var attrValue = obj[key];
-            console.log(attrName);
+            // if the program reads a key of type "type" that isn't listed in the current domain, skip
             if (attrName == "type" && attrValue != domain && domain != null) break;
-            /* send name value, description, photos, link (if applicable) */
+            // send name value, description, photos, link (if applicable)
             switch (attrName) {
                 case "title":
                     var title = document.createElement("h1");
@@ -37,12 +42,13 @@ async function populate(domain) {
                     link.textContent = attrValue;
                     link.setAttribute("href", attrValue);
                     project.appendChild(link);
+                    project.appendChild(document.createElement("hr"));
                     break;
                 default:
                     break;
             }
         }
-        project.appendChild(document.createElement("hr"));
+        // add filled project div to final dynamic div
         projectView.appendChild(project);
     }
 }
@@ -50,7 +56,8 @@ async function populate(domain) {
 /* helper function to receive JSON file via fetch for inclusion */
 /* TODO: change fetch request to local path (URL is only for local testing) */
 async function getJSON() {
-    const response = await fetch('https://proletariant.github.io/portfolio_website/scripts/main.json');
+    const response = await fetch('https://proletariant.github.io/portfolio_website/dbs/main.json');
+    //const response = await fetch('/dbs/main.json');
 
     const data = response.json();
 
